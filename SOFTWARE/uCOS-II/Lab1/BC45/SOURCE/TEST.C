d601acc94924fab15296e612f9acc8b6656c5f1f
Chou,Kuan-Lin <eva41121@gmail.com>
2019-06-11 20:17:59 +0800

    Initial commit

---
+++ SOFTWARE/uCOS-II/Lab1/BC45/SOURCE/TEST.C
@@ -0,0 +1,170 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*                           (c) Copyright 1992-2002, Jean J. Labrosse, Weston, FL
+*                                           All Rights Reserved
+*
+*                                               EXAMPLE #1
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+
+/*
+*********************************************************************************************************
+*                                               CONSTANTS
+*********************************************************************************************************
+*/
+
+#define  TASK_STK_SIZE                 512       /* Size of each task's stacks (# of WORDs)            */
+#define  N_TASKS                         2       /* Number of identical tasks                          */
+
+/*
+*********************************************************************************************************
+*                                               VARIABLES
+*********************************************************************************************************
+*/
+
+OS_STK        TaskStk[N_TASKS][TASK_STK_SIZE];        /* Tasks stacks                                  */
+OS_STK        TaskStartStk[TASK_STK_SIZE];
+char          TaskData[N_TASKS];                      /* Parameters to pass to each task               */
+OS_EVENT     *RandomSem;
+
+/*
+*********************************************************************************************************
+*                                           FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+        void  Task1(void *data);                       /* Function prototypes of tasks                  */
+        void  Task2(void *data);                       /* Function prototypes of tasks                  */
+        void  TaskStart(void *data);                  /* Function prototypes of Startup task           */
+static  void  TaskStartCreateTasks(void);
+static  void  TaskStartDispInit(void);
+static  void  TaskStartDisp(void);
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                                MAIN
+*********************************************************************************************************
+*/
+
+void  main (void)
+{
+    PC_DispClrScr(DISP_FGND_WHITE + DISP_BGND_BLACK);      /* Clear the screen                         */
+
+    OSInit();                                              /* Initialize uC/OS-II                      */
+
+    PC_DOSSaveReturn();                                    /* Save environment to return to DOS        */
+    PC_VectSet(uCOS, OSCtxSw);                             /* Install uC/OS-II's context switch vector */
+
+    RandomSem   = OSSemCreate(1);                          /* Random number semaphore                  */
+
+    OSTaskCreate(TaskStart, (void *)0, &TaskStartStk[TASK_STK_SIZE - 1], 0);
+    OSStart();                                             /* Start multitasking                       */
+}
+
+
+/*
+*********************************************************************************************************
+*                                              STARTUP TASK
+*********************************************************************************************************
+*/
+void  TaskStart (void *pdata)
+{
+#if OS_CRITICAL_METHOD == 3                                /* Allocate storage for CPU status register */
+    OS_CPU_SR  cpu_sr;
+#endif
+    char       s[100];
+    INT16S     key;
+
+
+    pdata = pdata;                                         /* Prevent compiler warning                 */
+
+    OS_ENTER_CRITICAL();
+    PC_VectSet(0x08, OSTickISR);                           /* Install uC/OS-II's clock tick ISR        */
+    PC_SetTickRate(OS_TICKS_PER_SEC);                      /* Reprogram tick rate                      */
+    OS_EXIT_CRITICAL();
+
+    OSStatInit();                                          /* Initialize uC/OS-II's statistics         */
+
+    TaskStartCreateTasks();                                /* Create all the application tasks         */
+
+    for (;;) {
+        if (PC_GetKey(&key) == TRUE) {                     /* See if key has been pressed              */
+            if (key == 0x1B) {                             /* Yes, see if it's the ESCAPE key          */
+                PC_DOSReturn();                            /* Return to DOS                            */
+            }
+        }
+
+        OSCtxSwCtr = 0;                                    /* Clear context switch counter             */
+        OSTimeDlyHMSM(0, 0, 1, 0);                         /* Wait one second                          */
+    }
+}
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                             CREATE TASKS
+*********************************************************************************************************
+*/
+
+static  void  TaskStartCreateTasks (void)
+{
+    OSTaskCreate(Task1, (void *)0, &TaskStk[0][TASK_STK_SIZE - 1], 1);
+    OSTaskCreate(Task2, (void *)0, &TaskStk[1][TASK_STK_SIZE - 1], 2);
+}
+
+/*
+*********************************************************************************************************
+*                                                  TASKS
+*********************************************************************************************************
+*/
+
+void  Task1 ()
+{
+    int start;
+    int end;
+    int toDelay;
+
+
+    OSTCBCur -> compTime = 1;
+    OSTCBCur -> period = 3;
+    printf("%d, %d, %d, %p\n", OSTCBCur -> compTime, OSTCBCur -> period, OSTCBCur);
+    start = OSTimeGet();
+    while (1) {
+        while ((OSTCBCur -> compTime) > 0) { 
+            OSTCBCur -> compTime--;
+        }
+        end = OSTimeGet();
+        toDelay = (OSTCBCur -> period) - (end - start);
+        start = start + (OSTCBCur -> period);
+        (OSTCBCur -> compTime) = 1;
+        OSTimeDly(toDelay);
+    }
+}
+
+void  Task2 ()
+{
+    int start;
+    int end;
+    int toDelay;
+
+    OSTCBCur -> compTime = 3;
+    OSTCBCur -> period = 6;
+    printf("%d, %d, %p\n", OSTCBCur -> compTime, OSTCBCur -> period, OSTCBCur);
+    start = OSTimeGet();
+    while (1) {
+        while ((OSTCBCur -> compTime) > 0) { 
+            OSTCBCur -> compTime--;
+        }
+        end = OSTimeGet();
+        toDelay = (OSTCBCur -> period) - (end - start);
+        start = start + (OSTCBCur -> period);
+        (OSTCBCur -> compTime) = 3;
+        OSTimeDly(toDelay);
+    }
+}
